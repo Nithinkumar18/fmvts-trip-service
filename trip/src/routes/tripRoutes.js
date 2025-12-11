643167f4e6d01310975da882514ce9584b8be48f
Nithin <vooturinithinkumar@gmail.com>
2025-12-11 17:30:36 +0530

    implement routes

---
+++ trip/src/routes/tripRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const tripController = require('../controller/tripController');
+const {validateUserRole} = require('../middleware/authorizeRole');
+
+router.post('/initiate',validateUserRole(["fleet_manager"]),tripController.initiateTrip);
+router.get('/trips',validateUserRole(["fleet_manager","admin"]),tripController.viewTrips);
+router.put('/updatetrip/:tId',validateUserRole(["fleet_manager"]),tripController.updateTripDetails);
+router.put('/status/:t_id',validateUserRole(["fleet_manager"]),tripController.tripStatusUp);
+router.delete('/retire/:id',validateUserRole(["fleet_manager","admin"]),tripController.deleteTrip);
+
+module.exports = router;
+
+
